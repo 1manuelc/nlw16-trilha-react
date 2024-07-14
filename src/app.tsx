4c6fcd7c816410482ac01d045063ade57205f0d3
1manuelc <manuelwn21@gmail.com>
2024-07-14 01:36:50 -0300

    refactor: router line unbreak

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,8 @@ import { CreateTripPage } from './pages/create-trip';
 import { TripDetailsPage } from './pages/trip-details';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <CreateTripPage />,
-	},
-	{
-		path: '/trips/:tripId',
-		element: <TripDetailsPage />,
-	},
+	{ path: '/', element: <CreateTripPage /> },
+	{ path: '/trips/:tripId', element: <TripDetailsPage /> },
 ]);
 
 export function App() {
