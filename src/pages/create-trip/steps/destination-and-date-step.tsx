8a9ee723c7e228281ec325ed02f3d33c8259ce4d
1manuelc <manuelwn21@gmail.com>
2024-07-15 17:17:32 -0300

    feat: dayPicker modal

--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -2,28 +2,48 @@ import { MapPin, Calendar, Settings2, ArrowRight } from 'lucide-react';
 import Button from '../../../components/button';
 import InvisibleInput from '../../../components/invisible-input';
 import VerticalSeparator from '../../../components/vertical-separator';
-// import { useState } from 'react';
+import { useState } from 'react';
+import Modal from '../../../components/modal';
+import { DateRange, DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { format } from 'date-fns';
 
 interface DestinationAndDateStepProps {
 	isGuestsInputOpen: boolean;
 	closeGuestsInput: () => void;
 	openGuestsInput: () => void;
+	setDestination: (destination: string) => void;
+	eventStartAndEndDates: DateRange | undefined;
+	setEventStartAndEndDates: (dates: DateRange | undefined) => void;
 }
 
 const DestinationAndDateStep: React.FC<DestinationAndDateStepProps> = ({
 	isGuestsInputOpen,
 	closeGuestsInput,
 	openGuestsInput,
+	setDestination,
+	setEventStartAndEndDates,
+	eventStartAndEndDates,
 }) => {
-	// const [isDayPickerOpen, setIsDayPickerOpen] = useState<boolean>(false);
+	const [isDayPickerModalOpen, setIsDayPickerModalOpen] =
+		useState<boolean>(false);
 
-	// function openDayPicker() {
-	// 	setIsDayPickerOpen(true);
-	// }
+	const displayedDate =
+		eventStartAndEndDates &&
+		eventStartAndEndDates.from &&
+		eventStartAndEndDates.to
+			? format(eventStartAndEndDates.from, "d' de 'LLL")
+					.concat(' até ')
+					.concat(format(eventStartAndEndDates.to, "d' de 'LLL"))
+			: null;
 
-	// function closeDayPicker() {
-	// 	setIsDayPickerOpen(false);
-	// }
+	function openDayPickerModal() {
+		setIsDayPickerModalOpen(true);
+	}
+
+	function closeDayPickerModal() {
+		setIsDayPickerModalOpen(false);
+	}
 
 	return (
 		<div className='h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3'>
@@ -32,16 +52,20 @@ const DestinationAndDateStep: React.FC<DestinationAndDateStepProps> = ({
 				type='text'
 				placeholder='Para onde você vai?'
 				width_variant='full'
+				onChange={(event) => setDestination(event.target.value)}
 			>
 				<MapPin className='size-5 text-zinc-400' />
 			</InvisibleInput>
 
 			<button
-				// disabled={isDayPickerOpen}
-				className='flex items-center gap-2 text-left w-40'
+				disabled={isGuestsInputOpen}
+				onClick={openDayPickerModal}
+				className='flex items-center gap-2 text-left w-[240px]'
 			>
 				<Calendar className='size-5 text-zinc-400' />
-				<span className='text-lg text-zinc-400'>Quando?</span>
+				<span className='text-lg text-zinc-400 flex-1'>
+					{displayedDate || 'Quando?'}
+				</span>
 			</button>
 
 			<VerticalSeparator />
@@ -56,6 +80,20 @@ const DestinationAndDateStep: React.FC<DestinationAndDateStepProps> = ({
 					<ArrowRight className='size-5' />
 					Continuar
 				</Button>
+			)}
+
+			{isDayPickerModalOpen && (
+				<Modal
+					title='Selecione a data'
+					closeModalFn={closeDayPickerModal}
+					size='small'
+				>
+					<DayPicker
+						mode='range'
+						selected={eventStartAndEndDates}
+						onSelect={setEventStartAndEndDates}
+					/>
+				</Modal>
 			)}
 		</div>
 	);
