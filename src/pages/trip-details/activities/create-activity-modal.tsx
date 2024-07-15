0db2504d221cf73a624a3ff05f03f9c99a29aad4
1manuelc <manuelwn21@gmail.com>
2024-07-15 19:32:19 -0300

    feat: create activity functional request

--- src/pages/trip-details/activities/create-activity-modal.tsx
+++ src/pages/trip-details/activities/create-activity-modal.tsx
@@ -2,6 +2,9 @@ import { Tag, Calendar } from 'lucide-react';
 import Button from '../../../components/button';
 import BoxInput from '../../../components/box-input';
 import Modal from '../../../components/modal';
+import { FormEvent } from 'react';
+import { api } from '../../../lib/axios';
+import { useParams } from 'react-router-dom';
 
 interface CreateActivityModalProps {
 	closeCreateActivityModal: () => void;
@@ -10,13 +13,30 @@ interface CreateActivityModalProps {
 const CreateActivityModal: React.FC<CreateActivityModalProps> = ({
 	closeCreateActivityModal,
 }) => {
+	const { tripId } = useParams();
+
+	async function createActivity(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		const data = new FormData(event.currentTarget);
+		const title = data.get('title')?.toString();
+		const occurs_at = data.get('occurs_at')?.toString();
+
+		api.post(`/trips/${tripId}/activities`, {
+			title,
+			occurs_at,
+		});
+
+		closeCreateActivityModal();
+	}
+
 	return (
 		<Modal
 			title='Cadastrar atividade'
 			description='Todos os convidados podem ver as atividades.'
 			closeModalFn={closeCreateActivityModal}
 		>
-			<form className='space-y-3'>
+			<form onSubmit={createActivity} className='space-y-3'>
 				<BoxInput type='text' name='title' placeholder='Qual atividade?'>
 					<Tag className='text-zinc-400 size-5' />
 				</BoxInput>
