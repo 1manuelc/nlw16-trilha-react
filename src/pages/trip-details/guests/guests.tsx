affd6dcc0a8a84f1ed5a8ea461de6c66e4cd01e6
1manuelc <manuelwn21@gmail.com>
2024-07-15 17:18:49 -0300

    feat: show guests feature

--- src/pages/trip-details/guests/guests.tsx
+++ src/pages/trip-details/guests/guests.tsx
@@ -1,26 +1,41 @@
 import { UserCog } from 'lucide-react';
 import Button from '../../../components/button';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import GuestItem from '../guests/guest-item';
+import { useParams } from 'react-router-dom';
+import { api } from '../../../lib/axios';
+import { Participant } from '../../../interfaces/participant';
 
 interface GuestsProps {
 	children?: ReactNode;
 }
 
 const Guests: React.FC<GuestsProps> = () => {
+	const [participants, setParticipants] = useState<Participant[]>([]);
+	const { tripId } = useParams();
+
+	useEffect(() => {
+		api
+			.get(`/trips/${tripId}/participants`)
+			.then((response) => setParticipants(response?.data.participants));
+	}, [tripId]);
+
 	return (
 		<div>
 			<div className='space-y-6'>
 				<h2 className='font-semibold text-xl'>Convidados</h2>
 
 				<div className='space-y-5'>
-					<GuestItem
-						name='Jessica White'
-						email='jes@white.com'
-						isConfirmed={true}
-					/>
-
-					<GuestItem name='John Doe' email='john@doe.com' isConfirmed={false} />
+					{participants.map((participant, index) => {
+						return (
+							<GuestItem
+								key={participant.id}
+								name={participant.name || `Convidado ${index}`}
+								email={participant.email}
+								isConfirmed={participant.is_confirmed}
+							/>
+						);
+					})}
 				</div>
 
 				<Button variant='secondary' size='full'>
