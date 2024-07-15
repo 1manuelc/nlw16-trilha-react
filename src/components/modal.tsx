93508ddbb31e151ca9566ec1ebff33066c45d0d8
1manuelc <manuelwn21@gmail.com>
2024-07-15 17:17:00 -0300

    feat: add modal variants

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,21 @@
 import { X } from 'lucide-react';
 import { ReactNode } from 'react';
+import { tv, VariantProps } from 'tailwind-variants';
 
-interface ModalProps {
+const modalVariants = tv({
+	base: 'rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5',
+	variants: {
+		size: {
+			default: 'w-[640px]',
+			small: 'w-fit',
+		},
+	},
+	defaultVariants: {
+		size: 'default',
+	},
+});
+
+interface ModalProps extends VariantProps<typeof modalVariants> {
 	title: string;
 	description?: ReactNode;
 	children?: ReactNode;
@@ -13,10 +27,11 @@ const Modal: React.FC<ModalProps> = ({
 	description,
 	children,
 	closeModalFn,
+	size,
 }) => {
 	return (
 		<div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-			<div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+			<div className={modalVariants({ size: size })}>
 				<div className='space-y-2'>
 					<div className='flex items-center justify-between'>
 						<h2 className='text-lg font-semibold'>{title}</h2>
